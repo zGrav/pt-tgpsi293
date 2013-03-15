1e26607602ca7611aecad4a09c8d418761ea03c8
David Samuel <zgrav@null.net>
2013-03-15 19:48:39 +0000

    Codename Breeze - Minor add
    Author tag.

--- Codename Breeze/codename_breeze/src/main/java/z/codename_breeze/gui/showDeviceInfo.java
+++ Codename Breeze/codename_breeze/src/main/java/z/codename_breeze/gui/showDeviceInfo.java
@@ -19,6 +19,16 @@ import javax.swing.table.AbstractTableModel;
 
 import com.android.ddmlib.IDevice;
 
+/*
+ * @author David "z" Samuel"
+ * 
+ * Don't steal Source Code, read it, learn it, adapt it and give credit to original owner.
+ * Check out my Github if you're interested on any other project that I might have - http://github.com/zGrav
+ * Contact me at zgrav@null.net for more information
+ * Please contribute and report bugs or any new features you would like to be implemented.
+ * 
+ * (c) z/David Samuel - 2nd February 2013
+ */
 
 public class showDeviceInfo extends JFrame {
 	
